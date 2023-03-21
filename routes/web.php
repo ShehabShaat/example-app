a3093ced36d00594b973b5c156a124c9b3d93cc9
shehabSh <shehab.e.sh@gmail.com>
2023-03-21 08:28:40 +0200

    tasks & about views

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('about', function () {
+    $name = 'Ahmed';
+   // return view('about', ['name' => 'Ahmed']);
+   // return view('about')->with(['name', $name]);
+    return view('about',compact('name'));
+});
+
+
+Route::post('about', function () {
+    $name = 'Ahmed';
+    if(isset($_POST['name']))
+        $name = $_POST['name'];
+    return view('about',compact('name'));
+});
+
+Route::get('tasks', function () {
+    $tasks = ['Task #1', 'Task #2', 'Task #3'];
+    return view('tasks', compact('tasks'));
+});
